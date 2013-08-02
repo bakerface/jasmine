1752c9a8b99bb12b11aa0405ca51b8d9cf3e6040
Christopher M. Baker <mail.chris.baker@gmail.com>
2013-08-02 17:14:01 -0400

    Added ignore to mark tests without expects

--- jasmine.h
+++ jasmine.h
@@ -1,23 +1,24 @@
 /*
+ * The MIT License (MIT)
+ * 
  * Copyright (c) 2013 Christopher M. Baker
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  */
 
@@ -31,6 +32,10 @@
 #define JASMINE_STATE_AFTER    2
 #define JASMINE_STATE_FINISHED 3
 
+#define JASMINE_GROUP_FAILED   0
+#define JASMINE_GROUP_PASSED   1
+#define JASMINE_GROUP_IGNORED   2
+
 typedef struct jasmine {
     unsigned char state;
     unsigned char group;
@@ -38,16 +43,20 @@ typedef struct jasmine {
     unsigned short last;
     unsigned short passed;
     unsigned short failed;
+    unsigned short ignored;
+    unsigned short expects;
     const char *reason;
 } jasmine_t;
 
 #define jasmine_init(jasmine) do { \
     (jasmine)->state = JASMINE_STATE_BEFORE; \
-    (jasmine)->group = 0; \
+    (jasmine)->group = JASMINE_GROUP_IGNORED; \
     (jasmine)->current = 0; \
     (jasmine)->last = 0; \
     (jasmine)->passed = 0; \
     (jasmine)->failed = 0; \
+    (jasmine)->ignored = 0; \
+    (jasmine)->expects = 0; \
     (jasmine)->reason = 0; \
 } while (0)
 
@@ -63,17 +72,22 @@ typedef struct jasmine {
         (jasmine)->state = JASMINE_STATE_IT)
 
 #define jasmine_it(jasmine, should) \
-    for ((jasmine)->group = 1, \
+    for ((jasmine)->group = JASMINE_GROUP_IGNORED, \
             (jasmine)->last = (unsigned short) ((jasmine)->last < __LINE__ \
             ? __LINE__ : (jasmine)->last); \
         JASMINE_STATE_IT == (jasmine)->state \
             && (jasmine)->current < __LINE__ \
             && (__LINE__ == ((jasmine)->current = __LINE__)); \
         (jasmine)->state = JASMINE_STATE_AFTER, \
-            ((jasmine)->group \
-            ? ((jasmine)->passed++, puts(" ✓ " should), 1) \
-            : ((jasmine)->failed++, printf(" ✗ " should ": expected %s\r\n", \
-                (jasmine)->reason), 1)))
+            ((jasmine)->group == JASMINE_GROUP_IGNORED \
+            ? ((jasmine)->ignored++, \
+                puts(" ! " should), JASMINE_GROUP_IGNORED) \
+            : ((jasmine)->group == JASMINE_GROUP_PASSED \
+                ? ((jasmine)->passed++, \
+                    puts(" ✓ " should), JASMINE_GROUP_PASSED) \
+                : ((jasmine)->failed++, \
+                    printf(" ✗ " should ": expected %s\r\n", \
+                        (jasmine)->reason), JASMINE_GROUP_FAILED))))
 
 #define jasmine_after(jasmine) \
     for (; JASMINE_STATE_AFTER == (jasmine)->state; \
@@ -82,9 +96,14 @@ typedef struct jasmine {
 
 #define jasmine_expect(jasmine, condition) \
     if (!(condition)) { \
-        (jasmine)->group = 0; \
+        (jasmine)->expects++; \
+        (jasmine)->group = JASMINE_GROUP_FAILED; \
         (jasmine)->reason = #condition; \
         continue; \
+    } \
+    else { \
+        (jasmine)->group = JASMINE_GROUP_PASSED; \
+        (jasmine)->expects++; \
     }
 
 #endif /* JASMINE_H_ */
