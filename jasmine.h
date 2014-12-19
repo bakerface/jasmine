1dec925124c5a5340ee371a1adbae6fbe2168c77
Christopher M. Baker <mail.chris.baker@gmail.com>
2014-12-19 12:42:35 -0500

    Before is optional

--- jasmine.h
+++ jasmine.h
@@ -6,11 +6,11 @@
 #ifndef JASMINE_H
 #define JASMINE_H
 
+#include <stdio.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-#include <stdio.h>
 
 enum {
   S_BEFORE,
@@ -23,13 +23,13 @@ enum {
 };
 
 struct {
-  unsigned s, g, c, l, a;
   const char *r;
+  unsigned s, g, c, l, a;
 } _;
 
 #define describe(x) \
 for (_.s = S_BEFORE, _.a = _.c = _.l = 0, puts(x); _.s != S_DONE; \
-  _.s = (_.a == 0 && _.s == S_BEFORE ? S_IT : _.s), \
+  _.s = (_.a == 0 && _.s == S_BEFORE && ++_.a ? S_IT : _.s), \
   _.s = (_.a == 0 && _.s == S_AFTER && _.c == _.l ? S_DONE : _.s), \
   _.s = (_.a == 0 && _.s == S_AFTER && _.c != _.l ? S_BEFORE : _.s), \
   _.s = (_.a == 0 && _.s == S_IT ? S_AFTER : _.s), \
@@ -45,9 +45,9 @@ for (; _.s == S_AFTER && ++_.a; _.s = (_.c == _.l ? S_DONE : S_BEFORE))
 for (_.g = G_IGNORE, _.l = (_.l > __LINE__ ? _.l : __LINE__); \
   _.s == S_IT && _.c < __LINE__ && __LINE__ == (_.c = __LINE__) && ++_.a; \
   _.s = S_AFTER, \
-  _.g == G_IGNORE && puts("  [ ] " x), \
-  _.g == G_PASS && puts("  [+] " x), \
-  _.g == G_FAIL && printf("  [-] " x ", expected: %s\n", _.r))
+  _.g == G_IGNORE ? puts("  [ ] " x) : 0, \
+  _.g == G_PASS ? puts("  [+] " x) : 0, \
+  _.g == G_FAIL ? printf("  [-] " x ", expected: %s\n", _.r) : 0)
 
 #define expect(x) \
 if (x) { \
