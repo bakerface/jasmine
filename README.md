32eac24ecbb24ad1b4c703d581fc50fa1a9bce77
Christopher M. Baker <mail.chris.baker@gmail.com>
2014-12-19 07:49:18 +0000

    Updated to simpler interface

--- README.md
+++ README.md
@@ -1,64 +1,48 @@
-jasmine
-=======
+# jasmine
 
 A port of the Jasmine test framework to ANSI C.
 This test framework is intended to be a minimalistic, bare-bones framework.
 In fact, the framework only consists of a single header file that contains a few macros.
 A simple example for how to use the framework is below.
 
-~~~~ c
-void example_test(jasmine_t *jasmine) {
-	example_t example;
-
-	jasmine_describe(jasmine, "an example") {
-		jasmine_before(jasmine) {
-			/* this will get called once before each 'it' */
-			example_new(&example);
-		}
-
-		jasmine_after(jasmine) {
-			/* this will get called once after each 'it' */
-			example_free(&example);
-		}
-
-		jasmine_it(jasmine, "should be able to pass") {
-			/* this will get called first */
-			jasmine_expect(jasmine, example_pass(&example));
-		}
-
-		jasmine_it(jasmine, "should be able to fail") {
-			/* this will get called second */
-			jasmine_expect(jasmine, example_fail(&example));
-		}
-		
-		jasmine_it(jasmine, "should be able to ignore") {
-			/* this will get called third */
-			/* there is no expect in here so the test is ignored */
-		}
-	}
-}
+~~~ c
+#include "jasmine.h"
 
 int main(void) {
-	jasmine_t jasmine;
-	jasmine_init(&jasmine);
-  
-  	/* run each of the tests here */
-	example_test(&jasmine);
+  describe("a test") {
+    
+    /* optional: called before each 'it' */
+    before {
+      i = 0;
+    }
 
-	printf("jasmine: %u passed, %u failed, %u ignored, %u expects\r\n",
-        	jasmine.passed, jasmine.failed, jasmine.ignored, jasmine.expects);
+    /* optional: called after each 'it' */
+    after {
+      i++;
+    }
 
-	return jasmine.failed;
+    it("should be able to pass") {
+      expect(i == 0);
+    }
+
+    it("should be able to fail") {
+      expect(i == 1);
+    }
+
+    it("should be able to ignore") {
+
+    }
+  }
+
+	return 0;
 }
-~~~~
+~~~
 
-Running the example will output each test with a pass or fail as well as total test statistics.
+Running the example will output each test with a pass, fail, or ignore.
 
-<pre>
-an example
- ✓ should be able to pass
- ✗ should be able to fail: expected example_fail(&example)
- ! should be able to ignore
-
-jasmine: 1 passed, 1 failed, 1 ignored, 2 expects
-</pre>
+```
+a test
+ [+] should be able to pass
+ [-] should be able to fail, expected: i == 1
+ [ ] should be able to ignore
+```
--- jasmine.h
+++ jasmine.h
@@ -1,109 +1,67 @@
 /*
- * The MIT License (MIT)
- * 
- * Copyright (c) 2013 Christopher M. Baker
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * Copyright (c) 2014 Christopher M. Baker
  *
  */
 
-#ifndef JASMINE_H_
-#define JASMINE_H_
+#ifndef JASMINE_H
+#define JASMINE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #include <stdio.h>
 
-#define JASMINE_STATE_BEFORE   0
-#define JASMINE_STATE_IT       1
-#define JASMINE_STATE_AFTER    2
-#define JASMINE_STATE_FINISHED 3
+enum {
+  S_BEFORE,
+  S_AFTER,
+  S_IT,
+  S_DONE,
+  G_IGNORE,
+  G_PASS,
+  G_FAIL
+};
 
-#define JASMINE_GROUP_FAILED   0
-#define JASMINE_GROUP_PASSED   1
-#define JASMINE_GROUP_IGNORED   2
+struct {
+  unsigned s, g, c, l, a;
+  const char *r;
+} _;
 
-typedef struct jasmine {
-    unsigned char state;
-    unsigned char group;
-    unsigned short current;
-    unsigned short last;
-    unsigned short passed;
-    unsigned short failed;
-    unsigned short ignored;
-    unsigned short expects;
-    const char *reason;
-} jasmine_t;
+#define describe(x) \
+for (_.s = S_BEFORE, _.a = _.c = _.l = 0, puts(x); _.s != S_DONE; \
+  _.s = (_.a == 0 && _.s == S_BEFORE ? S_IT : _.s), \
+  _.s = (_.a == 0 && _.s == S_AFTER && _.c == _.l ? S_DONE : _.s), \
+  _.s = (_.a == 0 && _.s == S_AFTER && _.c != _.l ? S_BEFORE : _.s), \
+  _.s = (_.a == 0 && _.s == S_IT ? S_AFTER : _.s), \
+  _.a = 0)
 
-#define jasmine_init(jasmine) do { \
-    (jasmine)->state = JASMINE_STATE_BEFORE; \
-    (jasmine)->group = JASMINE_GROUP_IGNORED; \
-    (jasmine)->current = 0; \
-    (jasmine)->last = 0; \
-    (jasmine)->passed = 0; \
-    (jasmine)->failed = 0; \
-    (jasmine)->ignored = 0; \
-    (jasmine)->expects = 0; \
-    (jasmine)->reason = 0; \
-} while (0)
+#define before \
+for (; _.s == S_BEFORE && ++_.a; _.s = S_IT)
 
-#define jasmine_describe(jasmine, what) \
-    for ((jasmine)->state = JASMINE_STATE_BEFORE, \
-            (jasmine)->current = 0, \
-            (jasmine)->last = 0, \
-            puts(what); \
-        (JASMINE_STATE_FINISHED == (jasmine)->state) ? (puts(""), 0) : 1; )
+#define after \
+for (; _.s == S_AFTER && ++_.a; _.s = (_.c == _.l ? S_DONE : S_BEFORE))
 
-#define jasmine_before(jasmine) \
-    for (; JASMINE_STATE_BEFORE == (jasmine)->state; \
-        (jasmine)->state = JASMINE_STATE_IT)
+#define it(x) \
+for (_.g = G_IGNORE, _.l = (_.l > __LINE__ ? _.l : __LINE__); \
+  _.s == S_IT && _.c < __LINE__ && __LINE__ == (_.c = __LINE__) && ++_.a; \
+  _.s = S_AFTER, \
+  _.g == G_IGNORE && puts("  [ ] " x), \
+  _.g == G_PASS && puts("  [+] " x), \
+  _.g == G_FAIL && printf("  [-] " x ", expected: %s\n", _.r))
 
-#define jasmine_it(jasmine, should) \
-    for ((jasmine)->group = JASMINE_GROUP_IGNORED, \
-            (jasmine)->last = (unsigned short) ((jasmine)->last < __LINE__ \
-            ? __LINE__ : (jasmine)->last); \
-        JASMINE_STATE_IT == (jasmine)->state \
-            && (jasmine)->current < __LINE__ \
-            && (__LINE__ == ((jasmine)->current = __LINE__)); \
-        (jasmine)->state = JASMINE_STATE_AFTER, \
-            ((jasmine)->group == JASMINE_GROUP_IGNORED \
-            ? ((jasmine)->ignored++, \
-                puts(" ! " should), JASMINE_GROUP_IGNORED) \
-            : ((jasmine)->group == JASMINE_GROUP_PASSED \
-                ? ((jasmine)->passed++, \
-                    puts(" ✓ " should), JASMINE_GROUP_PASSED) \
-                : ((jasmine)->failed++, \
-                    printf(" ✗ " should ": expected %s\r\n", \
-                        (jasmine)->reason), JASMINE_GROUP_FAILED))))
+#define expect(x) \
+if (x) { \
+  _.g = G_PASS; \
+} \
+else { \
+  _.g = G_FAIL; \
+  _.r = #x; \
+  continue; \
+}
 
-#define jasmine_after(jasmine) \
-    for (; JASMINE_STATE_AFTER == (jasmine)->state; \
-        (jasmine)->state = ((jasmine)->current == (jasmine)->last) \
-            ? JASMINE_STATE_FINISHED : JASMINE_STATE_BEFORE)
+#ifdef __cplusplus
+}
+#endif
 
-#define jasmine_expect(jasmine, condition) \
-    if (!(condition)) { \
-        (jasmine)->expects++; \
-        (jasmine)->group = JASMINE_GROUP_FAILED; \
-        (jasmine)->reason = #condition; \
-        continue; \
-    } \
-    else { \
-        (jasmine)->group = JASMINE_GROUP_PASSED; \
-        (jasmine)->expects++; \
-    }
+#endif /* JASMINE_H */
 
-#endif /* JASMINE_H_ */
